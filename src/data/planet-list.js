5e2266af5a491c3326059a21ff5aa8aef0caf0e8
Jahid0178 <jahidulhaque767@gmail.com>
2022-07-04 04:32:24 +0600

    complete home screen

---
+++ src/data/planet-list.js
@@ -0,0 +1,122 @@
+export const PLANET_LIST = [
+  {
+    name: "mercury",
+    color: "#def4fc",
+    description:
+      "Consectetur esse duis ipsum do veniam sint. Ea adipisicing aliquip ex deserunt eu est aliquip quis ad mollit. Et laboris id ea mollit ipsum anim incididunt voluptate duis consectetur eiusmod. Laborum ut sint id Lorem adipisicing. Non cillum aliquip qui ex excepteur minim ipsum deserunt pariatur aute tempor ut reprehenderit culpa. Minim sunt veniam incididunt aute consequat est deserunt dolore irure et mollit consequat. In reprehenderit nulla aliqua quis est dolor incididunt non.",
+    rotationTime: 58.6,
+    revolutionTime: "87.97 days",
+    radius: 2439.7,
+    avgTemp: "430°c",
+    wikiLink: "https://en.wikipedia.org/wiki/Mercury",
+    structure:
+      "Nulla excepteur exercitation velit in occaecat cupidatat veniam in ad et mollit. Irure laboris sunt sint fugiat voluptate exercitation nostrud. Esse nostrud quis elit deserunt ullamco ipsum consectetur nostrud. Est fugiat sint adipisicing reprehenderit",
+    surface:
+      "lorem sunt sint fugiat voluptate exercitation nostrud. Esse nostrud quis elit deserunt ullamco ipsum consectetur nostrud. Est fugiat sint adipisicing reprehenderit",
+  },
+  {
+    name: "venus",
+    color: "#f7cc7f",
+    description:
+      "Consectetur esse duis ipsum do veniam sint. Ea adipisicing aliquip ex deserunt eu est aliquip quis ad mollit. Et laboris id ea mollit ipsum anim incididunt voluptate duis consectetur eiusmod. Laborum ut sint id Lorem adipisicing. Non cillum aliquip qui ex excepteur minim ipsum deserunt pariatur aute tempor ut reprehenderit culpa. Minim sunt veniam incididunt aute consequat est deserunt dolore irure et mollit consequat. In reprehenderit nulla aliqua quis est dolor incididunt non.",
+    rotationTime: 58.6,
+    revolutionTime: "87.97 days",
+    radius: 2439.7,
+    avgTemp: "430°c",
+    wikiLink: "https://en.wikipedia.org/wiki/Mercury",
+    structure:
+      "Nulla excepteur exercitation velit in occaecat cupidatat veniam in ad et mollit. Irure laboris sunt sint fugiat voluptate exercitation nostrud. Esse nostrud quis elit deserunt ullamco ipsum consectetur nostrud. Est fugiat sint adipisicing reprehenderit",
+    surface:
+      "lorem sunt sint fugiat voluptate exercitation nostrud. Esse nostrud quis elit deserunt ullamco ipsum consectetur nostrud. Est fugiat sint adipisicing reprehenderit",
+  },
+  {
+    name: "earth",
+    color: "#def4fc",
+    description:
+      "Consectetur esse duis ipsum do veniam sint. Ea adipisicing aliquip ex deserunt eu est aliquip quis ad mollit. Et laboris id ea mollit ipsum anim incididunt voluptate duis consectetur eiusmod. Laborum ut sint id Lorem adipisicing. Non cillum aliquip qui ex excepteur minim ipsum deserunt pariatur aute tempor ut reprehenderit culpa. Minim sunt veniam incididunt aute consequat est deserunt dolore irure et mollit consequat. In reprehenderit nulla aliqua quis est dolor incididunt non.",
+    rotationTime: 58.6,
+    revolutionTime: "87.97 days",
+    radius: 2439.7,
+    avgTemp: "430°c",
+    wikiLink: "https://en.wikipedia.org/wiki/Mercury",
+    structure:
+      "Nulla excepteur exercitation velit in occaecat cupidatat veniam in ad et mollit. Irure laboris sunt sint fugiat voluptate exercitation nostrud. Esse nostrud quis elit deserunt ullamco ipsum consectetur nostrud. Est fugiat sint adipisicing reprehenderit",
+    surface:
+      "lorem sunt sint fugiat voluptate exercitation nostrud. Esse nostrud quis elit deserunt ullamco ipsum consectetur nostrud. Est fugiat sint adipisicing reprehenderit",
+  },
+  {
+    name: "mars",
+    color: "#ff6a45",
+    description:
+      "Consectetur esse duis ipsum do veniam sint. Ea adipisicing aliquip ex deserunt eu est aliquip quis ad mollit. Et laboris id ea mollit ipsum anim incididunt voluptate duis consectetur eiusmod. Laborum ut sint id Lorem adipisicing. Non cillum aliquip qui ex excepteur minim ipsum deserunt pariatur aute tempor ut reprehenderit culpa. Minim sunt veniam incididunt aute consequat est deserunt dolore irure et mollit consequat. In reprehenderit nulla aliqua quis est dolor incididunt non.",
+    rotationTime: 58.6,
+    revolutionTime: "87.97 days",
+    radius: 2439.7,
+    avgTemp: "430°c",
+    wikiLink: "https://en.wikipedia.org/wiki/Mercury",
+    structure:
+      "Nulla excepteur exercitation velit in occaecat cupidatat veniam in ad et mollit. Irure laboris sunt sint fugiat voluptate exercitation nostrud. Esse nostrud quis elit deserunt ullamco ipsum consectetur nostrud. Est fugiat sint adipisicing reprehenderit",
+    surface:
+      "lorem sunt sint fugiat voluptate exercitation nostrud. Esse nostrud quis elit deserunt ullamco ipsum consectetur nostrud. Est fugiat sint adipisicing reprehenderit",
+  },
+  {
+    name: "jupiter",
+    color: "#ecad7a",
+    description:
+      "Consectetur esse duis ipsum do veniam sint. Ea adipisicing aliquip ex deserunt eu est aliquip quis ad mollit. Et laboris id ea mollit ipsum anim incididunt voluptate duis consectetur eiusmod. Laborum ut sint id Lorem adipisicing. Non cillum aliquip qui ex excepteur minim ipsum deserunt pariatur aute tempor ut reprehenderit culpa. Minim sunt veniam incididunt aute consequat est deserunt dolore irure et mollit consequat. In reprehenderit nulla aliqua quis est dolor incididunt non.",
+    rotationTime: 58.6,
+    revolutionTime: "87.97 days",
+    radius: 2439.7,
+    avgTemp: "430°c",
+    wikiLink: "https://en.wikipedia.org/wiki/Mercury",
+    structure:
+      "Nulla excepteur exercitation velit in occaecat cupidatat veniam in ad et mollit. Irure laboris sunt sint fugiat voluptate exercitation nostrud. Esse nostrud quis elit deserunt ullamco ipsum consectetur nostrud. Est fugiat sint adipisicing reprehenderit",
+    surface:
+      "lorem sunt sint fugiat voluptate exercitation nostrud. Esse nostrud quis elit deserunt ullamco ipsum consectetur nostrud. Est fugiat sint adipisicing reprehenderit",
+  },
+  {
+    name: "saturn",
+    color: "#def4fc",
+    description:
+      "Consectetur esse duis ipsum do veniam sint. Ea adipisicing aliquip ex deserunt eu est aliquip quis ad mollit. Et laboris id ea mollit ipsum anim incididunt voluptate duis consectetur eiusmod. Laborum ut sint id Lorem adipisicing. Non cillum aliquip qui ex excepteur minim ipsum deserunt pariatur aute tempor ut reprehenderit culpa. Minim sunt veniam incididunt aute consequat est deserunt dolore irure et mollit consequat. In reprehenderit nulla aliqua quis est dolor incididunt non.",
+    rotationTime: 58.6,
+    revolutionTime: "87.97 days",
+    radius: 2439.7,
+    avgTemp: "430°c",
+    wikiLink: "https://en.wikipedia.org/wiki/Mercury",
+    structure:
+      "Nulla excepteur exercitation velit in occaecat cupidatat veniam in ad et mollit. Irure laboris sunt sint fugiat voluptate exercitation nostrud. Esse nostrud quis elit deserunt ullamco ipsum consectetur nostrud. Est fugiat sint adipisicing reprehenderit",
+    surface:
+      "lorem sunt sint fugiat voluptate exercitation nostrud. Esse nostrud quis elit deserunt ullamco ipsum consectetur nostrud. Est fugiat sint adipisicing reprehenderit",
+  },
+  {
+    name: "uranus",
+    color: "#def4fc",
+    description:
+      "Consectetur esse duis ipsum do veniam sint. Ea adipisicing aliquip ex deserunt eu est aliquip quis ad mollit. Et laboris id ea mollit ipsum anim incididunt voluptate duis consectetur eiusmod. Laborum ut sint id Lorem adipisicing. Non cillum aliquip qui ex excepteur minim ipsum deserunt pariatur aute tempor ut reprehenderit culpa. Minim sunt veniam incididunt aute consequat est deserunt dolore irure et mollit consequat. In reprehenderit nulla aliqua quis est dolor incididunt non.",
+    rotationTime: 58.6,
+    revolutionTime: "87.97 days",
+    radius: 2439.7,
+    avgTemp: "430°c",
+    wikiLink: "https://en.wikipedia.org/wiki/Mercury",
+    structure:
+      "Nulla excepteur exercitation velit in occaecat cupidatat veniam in ad et mollit. Irure laboris sunt sint fugiat voluptate exercitation nostrud. Esse nostrud quis elit deserunt ullamco ipsum consectetur nostrud. Est fugiat sint adipisicing reprehenderit",
+    surface:
+      "lorem sunt sint fugiat voluptate exercitation nostrud. Esse nostrud quis elit deserunt ullamco ipsum consectetur nostrud. Est fugiat sint adipisicing reprehenderit",
+  },
+  {
+    name: "neptune",
+    color: "#def4fc",
+    description:
+      "Consectetur esse duis ipsum do veniam sint. Ea adipisicing aliquip ex deserunt eu est aliquip quis ad mollit. Et laboris id ea mollit ipsum anim incididunt voluptate duis consectetur eiusmod. Laborum ut sint id Lorem adipisicing. Non cillum aliquip qui ex excepteur minim ipsum deserunt pariatur aute tempor ut reprehenderit culpa. Minim sunt veniam incididunt aute consequat est deserunt dolore irure et mollit consequat. In reprehenderit nulla aliqua quis est dolor incididunt non.",
+    rotationTime: 58.6,
+    revolutionTime: "87.97 days",
+    radius: 2439.7,
+    avgTemp: "430°c",
+    wikiLink: "https://en.wikipedia.org/wiki/Mercury",
+    structure:
+      "Nulla excepteur exercitation velit in occaecat cupidatat veniam in ad et mollit. Irure laboris sunt sint fugiat voluptate exercitation nostrud. Esse nostrud quis elit deserunt ullamco ipsum consectetur nostrud. Est fugiat sint adipisicing reprehenderit",
+    surface:
+      "lorem sunt sint fugiat voluptate exercitation nostrud. Esse nostrud quis elit deserunt ullamco ipsum consectetur nostrud. Est fugiat sint adipisicing reprehenderit",
+  },
+];
--- src/screens/home.js
+++ src/screens/home.js
@@ -1,14 +1,43 @@
-import { View, SafeAreaView, StatusBar, StyleSheet } from "react-native";
+import {
+  View,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  FlatList,
+} from "react-native";
 import React from "react";
 import Text from "../components/text/text";
 import PlanetHeader from "../components/planet-header";
 import { colors } from "../theme/colors";
+import { PLANET_LIST } from "../data/planet-list";
+import { spacing } from "../theme/spacing";
+import { AntDesign } from "@expo/vector-icons";
 
 export default function Home() {
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
       <PlanetHeader />
+      <FlatList
+        contentContainerStyle={styles.list}
+        data={PLANET_LIST}
+        keyExtractor={(item) => item.name}
+        renderItem={({ item }) => {
+          const { name, color } = item;
+          return (
+            <View style={styles.item}>
+              <View style={{ flexDirection: "row" }}>
+                <View style={[styles.circle, { backgroundColor: color }]} />
+                <Text style={styles.itemName} preset="h3">
+                  {name}
+                </Text>
+              </View>
+              <AntDesign name="right" size={18} color="white" />
+            </View>
+          );
+        }}
+        ItemSeparatorComponent={() => <View style={styles.separator} />}
+      />
     </SafeAreaView>
   );
 }
@@ -17,5 +46,27 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.black,
+  },
+  item: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: spacing[4],
+  },
+  itemName: {
+    textTransform: "uppercase",
+    marginLeft: spacing[4],
+  },
+  circle: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+  },
+  list: {
+    padding: spacing[4],
+  },
+  separator: {
+    borderBottomColor: colors.white,
+    borderWidth: 0.5,
   },
 });
